babc8433e6d4dddee297db2d269cb64fce0f4fa8
Yegor <fomenkoyegor@gmail.com>
2019-01-05 13:31:52 +0200

    Add files via upload

---
+++ kolechkintodo/precache-manifest.cffbdde256689b97c3200bf729f4afbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76fed279a5508195b88d",
+    "url": "/static/js/main.76fed279.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "76fed279a5508195b88d",
+    "url": "/static/css/main.8f00ec89.chunk.css"
+  },
+  {
+    "revision": "32ce5f18a97a978be6037f8e5c6592c1",
+    "url": "/index.html"
+  }
+];
